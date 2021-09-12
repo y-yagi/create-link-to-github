3d1c3481429bd48e94266c0aaca4895ee3778d63
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-09-12 09:07:10 +0900

    Use Clipboard API

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   }
 });
 
-function createLink() {
+async function createLink() {
   const url = document.URL;
   var title = '';
 
@@ -16,18 +16,5 @@ function createLink() {
     title = url.replace("https://github.com/", "")
   }
 
-  saveToClipboard(`[${title}](${url})`);
-}
-
-function saveToClipboard(str) {
-  var textArea = document.createElement("textarea");
-  textArea.style.cssText = "position:absolute;left:-100%";
-
-  document.body.appendChild(textArea);
-
-  textArea.value = str;
-  textArea.select();
-  document.execCommand("copy");
-
-  document.body.removeChild(textArea);
+  await navigator.clipboard.writeText(`[${title}](${url})`);
 }
