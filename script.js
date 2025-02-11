6b023fe5f559eeee248fc00905ffeec97721fee9
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2025-02-11 12:04:24 +0900

    Fix reading titles of commits and issues

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ async function createLink() {
   var title = '';
 
   if (url.includes('/commit/')) {
-    title = document.getElementsByClassName('commit-title')[0].textContent.trim();
-  } else if (url.includes('pull') || url.includes('issues') || url.includes('discussions')) {
+    title = document.getElementsByTagName("title")[0].textContent.trim();
+  } else if (url.includes('issues')) {
+    title = document.querySelectorAll('[data-testid="issue-title"]')[0].textContent.trim();
+  } else if (url.includes('pull') || url.includes('discussions')) {
     title = document.getElementsByClassName('js-issue-title')[0].textContent.trim();
   } else {
     title = url.replace("https://github.com/", "")
